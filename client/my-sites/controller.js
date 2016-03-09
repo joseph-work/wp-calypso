64e01709fdb68445d532191d6d2bad4ea9067ebb
Andrew Duthie <andrew@andrewduthie.com>
2016-03-09 16:18:55 -0500

    Sites: Render publish menu using post types state

--- client/my-sites/controller.js
+++ client/my-sites/controller.js
@@ -4,6 +4,7 @@
 var page = require( 'page' ),
 	ReactDom = require( 'react-dom' ),
 	React = require( 'react' );
+import { Provider as ReduxProvider } from 'react-redux';
 
 /**
  * Internal Dependencies
@@ -31,14 +32,16 @@ function renderNavigation( context, allSitesPath, siteBasePath ) {
 
 	// Render the My Sites navigation in #secondary
 	ReactDom.render(
-		React.createElement( NavigationComponent, {
-			layoutFocus: layoutFocus,
-			path: context.path,
-			allSitesPath: allSitesPath,
-			siteBasePath: siteBasePath,
-			user: user,
-			sites: sites
-		} ),
+		React.createElement( ReduxProvider, { store: context.store },
+			React.createElement( NavigationComponent, {
+				layoutFocus: layoutFocus,
+				path: context.path,
+				allSitesPath: allSitesPath,
+				siteBasePath: siteBasePath,
+				user: user,
+				sites: sites
+			} )
+		),
 		document.getElementById( 'secondary' )
 	);
 }
